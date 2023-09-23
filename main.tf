57c347272a71b9d1160cc0ef152895f4cf507360
Ramprasad V <vrprasad@gmail.com>
2023-09-23 12:16:18 +0530

    Added backend

--- main.tf
+++ main.tf
@@ -1,8 +1,4 @@
 provider "azurerm" {
-    subscription_id = "1c5a7ba3-fff1-4294-a3b2-6d8f9021c7ae"
-    client_id       = "2c7853fe-0b97-4d45-a570-53863487f13a"
-    client_secret   = "msA8Q~14d3xDhuJaYjSeZL31nh6u4P-smhSgkdf0"
-    tenant_id       = "87cf491e-5318-4e3b-895f-023a85778c76"
     features {}
 }
 
@@ -12,6 +8,12 @@ terraform {
         source = "hashicorp/azurerm"
         version = "2.42.0"
     }
+  }
+  backend "azurerm" {
+      resource_group_name  = "rp0041-rg"
+      storage_account_name = "rp0041sa"
+      container_name       = "tfstate"
+      key                  = "terraform.dev.tfstate"
   }
 }
 
