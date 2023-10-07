1cc657081df3fe73efa3fcf201a638596af2be3c
Ramprasad V <vrprasad@gmail.com>
2023-10-07 09:35:07 +0530

    Added TFS variables

--- main.tf
+++ main.tf
@@ -10,14 +10,27 @@ terraform {
     }
   }
   backend "azurerm" {
-      resource_group_name  = "rp0041-rg"
-      storage_account_name = "rp0041sa"
-      container_name       = "tfstate"
-      key                  = "terraform.dev.tfstate"
+      resource_group_name  = var.tfs_RG
+      storage_account_name = var.tfs_storageaccount
+      container_name       = var.tfs_container
+      key                  = var.tfs_key
   }
 }
+data "azurerm_key_vault" "rp0041-kv" {
+  name                = "rp0041-kv" // KeyVault name
+  resource_group_name = "rp0041-rg" // resourceGroup
+}
 
-resource "azurerm_resource_group" "rg" {
+data "azurerm_key_vault_secret" "kvsecret" {
+name = "secret-app-01" // Name of secret
+key_vault_id = data.azurerm_key_vault.rp0041-kv.id
+}
+
+/*resource "azurerm_resource_group" "rg" {
     name = var.resource_group_name
     location = var.res_location
+}*/
+
+output "key_from_vault" {
+  value     = azurerm_key_vault_secret.kvsecret
 }
--- variables.tf
+++ variables.tf
@@ -9,3 +9,35 @@ variable "res_location" {
     description = "Location"
     default = "west europe"
 }
+
+variable "tfs_RG" {
+    type = string
+    description = "TF State Resource Group"
+    default = "rp0041-rg"
+}
+
+variable "tfs_storageaccount" {
+    type = string
+    description = "TF State Storage account"
+    default = "rp0041sa"
+}
+
+variable "tfs_container" {
+    type = string
+    description = "TF State Container"
+    default = "tfstate"
+}
+
+variable "tfs_key" {
+    type = string
+    description = "TF State Key"
+    default = "terraform.dev.tfstate"
+}
+
+variable "KeyVault" {
+    type = string
+    description = "TF State Resource Group"
+    default = "rp0041-rg"
+}
+
+
